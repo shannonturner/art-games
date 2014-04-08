529e9e05d22afa17bf889a3558e5269ec3c041f9
Shannon Turner <shannon.v.turner@gmail.com>
2014-04-08 07:25:33 -0400

    Minor changes to handling failure

--- apps/mash/get_artworks.py
+++ apps/mash/get_artworks.py
@@ -26,12 +26,14 @@ def get_artworks(**kwargs):
     both_apis.append(random.choice(available_apis.keys()))
 
     for index, one_api in enumerate(both_apis):
-        while both_apis[index] == one_api and len(available_apis) > 0:
 
-            if len(available_apis) < 2:
-                time.sleep(1)
+        if len(available_apis) == 1:
+            time.sleep(1)
 
+        if len(available_apis > 1):
             both_apis[index], which_api = available_apis[one_api][0]().get_artwork()
+        else:
+            both_apis[index] = False
 
     # This will only happen in the rare instances that all available APIs are down. 
     if not both_apis[0] and not both_apis[1]:
@@ -41,8 +43,8 @@ def get_artworks(**kwargs):
         }
 
     # In the rare case of two images being identical, one_api still contains the right side's api to re-pull from
-    while both_apis[0] == both_apis[1] and not both_apis[1]:
-        both_apis[1] = available_apis[one_api]().get_artwork()
+    while both_apis[0] == both_apis[1] and both_apis[1]:
+        both_apis[1] = available_apis[one_api][0]().get_artwork()
 
     both_apis.append({'total_artworks': total_artworks_text})
 
