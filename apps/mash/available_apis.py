1604911cc149f78e19431549b03b43aa108f25ed
Shannon Turner <shannon.v.turner@gmail.com>
2014-04-13 11:17:05 -0400

    Added Brooklyn to available APIs

--- apps/mash/available_apis.py
+++ apps/mash/available_apis.py
@@ -1,10 +1,12 @@
 # Whenever adding a new museum API, be sure to add it here.
 # from apps.mash.museum_apis.luce_museum_api import LuceMuseumApi
+from apps.mash.museum_apis.brooklyn_museum_api import BrooklynMuseumApi
 # from apps.mash.museum_apis.rijks_museum_api import RijksMuseumApi
 from apps.mash.museum_apis.victoria_albert_museum_api import VictoriaAlbertMuseumApi
 
 available_apis = {
-    # 'luce': (LuceMuseumApi, 31114) # as of 2013 November
-    # 'rijksmuseum': RijksMuseumApi,
-    'victoriaalbertmuseum': (VictoriaAlbertMuseumApi, 402101) # as of 2014 Apr 7
+    # 'luce': (LuceMuseumApi, 31114), # as of 2013 November
+    'brooklyn': (BrooklynMuseumApi, 29043), # as of 2014 Apr 13
+    # 'rijksmuseum': (RijksMuseumApi, 177752), # as of 2014 Apr 13
+    'victoriaalbertmuseum': (VictoriaAlbertMuseumApi, 402101), # as of 2014 Apr 7
 }
